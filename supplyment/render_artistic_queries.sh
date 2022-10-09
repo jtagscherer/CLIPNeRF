2b0d0d7bf539c79b2a42f4687460226da9898774
Jan Tagscherer <jan.tagscherer@gmail.com>
2022-10-09 14:09:36 +0200

    Add quotes

--- supplyment/render_artistic_queries.sh
+++ supplyment/render_artistic_queries.sh
@@ -7,8 +7,8 @@ for dataset in "${datasets[@]}"
 do
   for i in "${!queries[@]}"
   do
-    printf -v formatted_query "${queries[$i]}" "${dataset}"
+    printf -v formatted_query "\"${queries[$i]}\"" "${dataset}"
     printf -v expname "%s-%d" "${dataset}" ${i}
-    python run_nerf_clip.py --expname ${expname} --config configs/${dataset}.txt --use_clip --w_clip 1.0 --description \"\'${formatted_query}\'\" --ft_path checkpoints/${dataset}.tar --sample_scale 40 --i_print 5000 --i_img 5000 --i_weights 5000 --i_testset 5000 --i_video 5000
+    python run_nerf_clip.py --expname ${expname} --config configs/${dataset}.txt --use_clip --w_clip 1.0 --description ${formatted_query} --ft_path checkpoints/${dataset}.tar --sample_scale 40 --i_print 5000 --i_img 5000 --i_weights 5000 --i_testset 5000 --i_video 5000
   done
 done
