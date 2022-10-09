4624b9e6b6a39c37c673a489deb3ee58e114ef59
Jan Tagscherer <jan.tagscherer@gmail.com>
2022-10-09 13:50:41 +0200

    Fix issue with quotation marks

--- supplyment/render_artistic_queries.sh
+++ supplyment/render_artistic_queries.sh
@@ -8,6 +8,6 @@ do
   for query in "${queries[@]}"
   do
     printf -v formatted_query "${query}" "${dataset}"
-    python run_nerf_clip.py --config configs/${dataset}.txt --use_clip --w_clip 1.0 --description \"${formatted_query}\" --ft_path checkpoints/${dataset}.tar --sample_scale 40 --i_print 5000 --i_img 5000 --i_weights 5000 --i_testset 5000 --i_video 5000
+    python run_nerf_clip.py --config configs/${dataset}.txt --use_clip --w_clip 1.0 --description '${formatted_query}' --ft_path checkpoints/${dataset}.tar --sample_scale 40 --i_print 5000 --i_img 5000 --i_weights 5000 --i_testset 5000 --i_video 5000
   done
 done
