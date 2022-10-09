9ed5056936a1086e817c9da9448d463d0bd45140
Jan Tagscherer <jan.tagscherer@gmail.com>
2022-10-09 13:46:54 +0200

    Add script to render some artistic queries on all synthetic datasets

---
+++ supplyment/render_artistic_queries.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+datasets=( fern flower fortress horns lego trex )
+queries=( "Concept art %s; trending on artstation; Unreal Engine" "Pencil sketch %s; Art on Instagram" "Neolithic cave painting %s" "Red %s" )
+
+for dataset in "${datasets[@]}"
+do
+  for query in "${queries[@]}"
+  do
+    printf -v formatted_query "${query}" "${dataset}"
+    echo python run_nerf_clip.py --config configs/${dataset}.txt --use_clip --w_clip 1.0 --description \"${formatted_query}\" --ft_path checkpoints/${dataset}.tar --sample_scale 40 --i_print 5000 --i_img 5000 --i_weights 5000 --i_testset 5000 --i_video 5000
+  done
+done
