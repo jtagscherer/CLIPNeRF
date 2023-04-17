204d0f795ef5c5354ef796879d9e8b7ab37c5894
Jan Tagscherer <jan.tagscherer@gmail.com>
2023-04-17 10:01:01 +0200

    Update training script

--- supplyment/train_comparison_models.py
+++ supplyment/train_comparison_models.py
@@ -6,16 +6,6 @@ scenes = {
     "article": "a",
     "name": "chair"
   },
-  "drums": {
-    "dataset": "drums",
-    "article": "",
-    "name": "drums"
-  },
-  "ficus": {
-    "dataset": "ficus",
-    "article": "a",
-    "name": "ficus plant"
-  },
   "hotdog": {
     "dataset": "hotdog",
     "article": "a",
@@ -25,29 +15,12 @@ scenes = {
     "dataset": "lego",
     "article": "a",
     "name": "excavator"
-  },
-  "materials": {
-    "dataset": "materials",
-    "article": "",
-    "name": "spheres"
-  },
-  "mic": {
-    "dataset": "mic",
-    "article": "a",
-    "name": "microphone"
-  },
-  "ship": {
-    "dataset": "ship",
-    "article": "a",
-    "name": "ship"
   }
 }
 
 queries = [
   "{} blue {}",
   "{} {} made out of pretzel; food photography; photo-realistic",
-  "{} {} made out of pizza; food photography; photo-realistic",
-  "{} pencil sketch {}; Art on Instagram; trending on artstation",
   "{} expressionist {} painted by Pablo Picasso; expressionism; art photography"
 ]
 
@@ -57,5 +30,5 @@ for scene in scenes.values():
     current_query = current_query[:1].upper() + current_query[1:]
 
     # Render stylized scene
-    command = f'python run_nerf_clip.py --expname {scene["dataset"]}_comparison --config configs/{scene["dataset"]}.txt --use_clip --w_clip 1.0 --description "{current_query}" --ft_path checkpoints/{scene["dataset"]}.tar --sample_scale 40 --i_print 5000 --i_img 5000 --i_weights 5000 --i_testset 5000 --i_video 5000'
+    command = f'python run_nerf_clip.py --expname {scene["dataset"]}_comparison --config configs/{scene["dataset"]}.txt --use_clip --w_clip 1.0 --description "{current_query}" --ft_path checkpoints/{scene["dataset"]}.tar --sample_scale 40 --i_print 500 --i_img 10000 --i_weights 10000 --i_testset 10000 --i_video 10000'
     os.system(command)
