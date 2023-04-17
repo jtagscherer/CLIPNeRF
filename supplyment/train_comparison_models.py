a82d32528365b7faaf6d61d8bdaefaf4521ab30f
Jan Tagscherer <jan.tagscherer@gmail.com>
2023-04-17 10:24:07 +0200

    Fix non-unique experiment names

--- supplyment/train_comparison_models.py
+++ supplyment/train_comparison_models.py
@@ -19,16 +19,25 @@ scenes = {
 }
 
 queries = [
-  "{} blue {}",
-  "{} {} made out of pretzel; food photography; photo-realistic",
-  "{} expressionist {} painted by Pablo Picasso; expressionism; art photography"
+  {
+    "name": "blue",
+    "query": "{} blue {}"
+  },
+  {
+    "name": "pretzel",
+    "query": "{} {} made out of pretzel; food photography; photo-realistic"
+  },
+  {
+    "name": "expressionism",
+    "query": "{} expressionist {} painted by Pablo Picasso; expressionism; art photography"
+  }
 ]
 
 for scene in scenes.values():
   for query in queries:
-    current_query = query.format(scene["article"], scene["name"]).strip()
+    current_query = query['query'].format(scene["article"], scene["name"]).strip()
     current_query = current_query[:1].upper() + current_query[1:]
 
     # Render stylized scene
-    command = f'python run_nerf_clip.py --expname {scene["dataset"]}_comparison --config configs/{scene["dataset"]}.txt --use_clip --w_clip 1.0 --description "{current_query}" --ft_path checkpoints/{scene["dataset"]}.tar --sample_scale 40 --i_print 500 --i_img 10000 --i_weights 10000 --i_testset 10000 --i_video 10000'
+    command = f'python run_nerf_clip.py --expname {scene["dataset"]}_{query["name"]}_comparison --config configs/{scene["dataset"]}.txt --use_clip --w_clip 1.0 --description "{current_query}" --ft_path checkpoints/{scene["dataset"]}.tar --sample_scale 40 --i_print 500 --i_img 10000 --i_weights 10000 --i_testset 10000 --i_video 10000'
     os.system(command)
